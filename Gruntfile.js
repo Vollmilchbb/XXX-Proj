5d9860fa74ff7af6d5b6f40969cc4096c8268008
Dmitry Brodskiy <dmitry.brodskiy@sab.sachsen.de>
2018-06-04 09:30:13 +0200

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,146 +1,193 @@
 
 module.exports = function(grunt) {
-	
-	// define constants
-	let sUser = grunt.option('user');
-	let sPwd = grunt.option('pwd');
-	
-	grunt.initConfig({
 
-		dir: {
-			webapp: 'webapp',
-			dist: 'dist',
-			bower_components: 'bower_components'
-		},
+    // define constants
+    let sUser = grunt.option('user');
+    let sPwd = grunt.option('pwd');
 
-		connect: {
-			options: {
-				port: 8080,
-				hostname: '*'
-			},
-			src: {},
-			dist: {}
-		},
+    let sonarHostUrl = grunt.option('sonar.host.url') || 'http://localhost:9000';
+    let sonarLogin = grunt.option('sonar.login');
+    let sonarPassword = grunt.option('sonar.password');
+    let sonarProjectKey = grunt.option('sonar.projectKey');
+    let sonarBranch = grunt.option('sonar.branch');
+    let sonarAnalysisMode = grunt.option('sonar.analysis.mode');
 
-		openui5_connect: {
-			options: {
-				resources: [
-					'<%= dir.bower_components %>/openui5-sap.ui.core/resources',
-					'<%= dir.bower_components %>/openui5-sap.m/resources',
-					'<%= dir.bower_components %>/openui5-sap.ui.layout/resources',
-					'<%= dir.bower_components %>/openui5-themelib_sap_belize/resources'
-				]
-			},
-			src: {
-				options: {
-					appresources: '<%= dir.webapp %>'
-				}
-			},
-			dist: {
-				options: {
-					appresources: '<%= dir.dist %>'
-				}
-			}
-		},
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-		openui5_preload: {
-			component: {
-				options: {
-					resources: {
-						cwd: '<%= dir.webapp %>',
-						prefix: 'todo'
-					},
-					dest: '<%= dir.dist %>'
-				},
-				components: true
-			}
-		},
+        dir: {
+            webapp: 'webapp',
+            dist: 'dist',
+            bower_components: 'bower_components'
+        },
 
-		clean: {
-			dist: '<%= dir.dist %>/'
-		},
+        connect: {
+            options: {
+                port: 8080,
+                hostname: '*'
+            },
+            src: {},
+            dist: {}
+        },
 
-		copy: {
-			dist: {
-				files: [ {
-					expand: true,
-					cwd: '<%= dir.webapp %>',
-					src: [
-						'**'
-					],
-					dest: '<%= dir.dist %>'
-				} ]
-			}
-		},
+        openui5_connect: {
+            options: {
+                resources: [
+                    '<%= dir.bower_components %>/openui5-sap.ui.core/resources',
+                    '<%= dir.bower_components %>/openui5-sap.m/resources',
+                    '<%= dir.bower_components %>/openui5-sap.ui.layout/resources',
+                    '<%= dir.bower_components %>/openui5-themelib_sap_belize/resources'
+                ]
+            },
+            src: {
+                options: {
+                    appresources: '<%= dir.webapp %>'
+                }
+            },
+            dist: {
+                options: {
+                    appresources: '<%= dir.dist %>'
+                }
+            }
+        },
 
-		eslint: {
-			options: {
-				configFile: '.eslintrc.json',
-			},
-			target: ['webapp/controller/*.js']
-		},
-		
-    	nwabap_ui5uploader: {
-    		options: {
-    	        conn: {
-    	        	server: 'http://sabvbsl2.sab-sap.os.itelligence.de:8000',
-    	        },
-    	        auth: {
-    	        	user: sUser,
-    	        	pwd: sPwd,
-    	        }
-    		},
-	        upload_build: {
-		    	options: {
-		    		ui5: {
-		    			package: 'ZDB_ANTRD_PRF',
-		    			bspcontainer: 'ZDB_APRF_APP',
-		    			bspcontainer_text: 'SAPUI5 Application: ZDBUI5_DEMO_APP',
-		    			transportno: 'BSGK900074'
-		    		},
-		    		resources: {
-		    			// the source which will be transported
-		    			cwd: 'dist',
-		    			src: '**/*.*'
-		    		}
-		    	}
-		    }
-    	}
-	});
-	
+        openui5_preload: {
+            component: {
+                options: {
+                    resources: {
+                        cwd: '<%= dir.webapp %>',
+                        prefix: 'todo'
+                    },
+                    dest: '<%= dir.dist %>'
+                },
+                components: true
+            }
+        },
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-openui5');
-	grunt.loadNpmTasks('grunt-nwabap-ui5uploader');
+        clean: {
+            dist: '<%= dir.dist %>/'
+        },
+
+        copy: {
+            dist: {
+                files: [ {
+                    expand: true,
+                    cwd: '<%= dir.webapp %>',
+                    src: [
+                        '**'
+                    ],
+                    dest: '<%= dir.dist %>'
+                } ]
+            }
+        },
+
+        eslint: {
+            options: {
+                configFile: '.eslintrc.json',
+            },
+            target: ['webapp/controller/*.js']
+        },
+
+        nwabap_ui5uploader: {
+            options: {
+                conn: {
+                    server: 'http://sabvbsl2.sab-sap.os.itelligence.de:8000',
+                },
+                auth: {
+                    user: sUser,
+                    pwd: sPwd,
+                }
+            },
+            upload_build: {
+                options: {
+                    ui5: {
+                        package: 'ZDB_ANTRD_PRF',
+                        bspcontainer: 'ZDB_APRF_APP',
+                        bspcontainer_text: 'SAPUI5 Application: ZDBUI5_DEMO_APP',
+                        transportno: 'BSGK900074'
+                    },
+                    resources: {
+                        // the source which will be transported
+                        cwd: 'dist',
+                        src: '**/*.*'
+                    }
+                }
+            }
+        },
+
+        sonarRunner: {
+            analysis: {
+                options: {
+                    debug: true,
+                    // separator: '\n',
+                    sonar: {
+                        host: {
+                            url: sonarHostUrl
+                        },
+                        login: sonarLogin,
+                        password: sonarPassword,
+                        sources: ['webapp'].join(','),
+                        projectKey: sonarProjectKey,
+                        branch: sonarBranch,
+                        projectName: '<%= pkg.name %>',
+                        projectVersion: '<%= pkg.version %>',
+                        language: 'js',
+                        sourceEncoding: 'UTF-8',
+                        analysis: {
+                            mode: sonarAnalysisMode
+                        },
+                        issuesReport: {
+                            json: {
+                                enable: true
+                            }
+                        },
+                        report: {
+                            export: {
+                                path: 'sonar-report.json'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-openui5');
+    grunt.loadNpmTasks('grunt-nwabap-ui5uploader');
     grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('@sabdd/grunt-sonar-runner');
 
 
+    // Server task
+    grunt.registerTask('serve', function(target) {
+        grunt.task.run('openui5_connect:' + (target || 'src') + ':keepalive');
+    });
 
-	// Server task
-	grunt.registerTask('serve', function(target) {
-		grunt.task.run('openui5_connect:' + (target || 'src') + ':keepalive');
-	});
+    // Linting task
+    grunt.registerTask('standard', ['standard']);
 
-	// Linting task
-	grunt.registerTask('standard', ['standard']);
-	
-	//deploy task
-	grunt.registerTask('deploy', ['nwabap_ui5uploader']);
+    //deploy task
+    grunt.registerTask('deploy', ['nwabap_ui5uploader']);
 
-	// Build task
-	grunt.registerTask('build', ['copy']);
+    // Build task
+    grunt.registerTask('build', ['copy']);
 
-	//Eslint task
-	grunt.registerTask('lint', ['eslint']);
+    //Eslint task
+    grunt.registerTask('lint', ['eslint']);
 
-	// Default task
-	grunt.registerTask('default', [
-		'eslint',
-		'clean',
-		'build',
-		'serve:dist'
-	]);
+    //Eslint task
+    grunt.registerTask('sonar', ['sonarRunner']);
+
+    // Default task
+    grunt.registerTask('default', [
+        'eslint',
+        'clean',
+        'build',
+        'serve:dist'
+    ]);
 };
--- webapp/controller/antragsdatenerfassung/AllgemeineAntragsdaten.controller.js
+++ webapp/controller/antragsdatenerfassung/AllgemeineAntragsdaten.controller.js
@@ -35,23 +35,3 @@ sap.ui.define([
 		});
 	}
 );
-///			let checkboxes = [
-//                  view.byId("chkbxAntrStlrAngstlt"),
-//                  view.byId("chkbxAntrStlr2Angstlt"),
-//                  view.byId("chkbxNatPerson"),
-//                  view.byId("chkbxKindergeldNachweis"),
-//                  view.byId("chkbxAntragVorhanden"),
-//                  view.byId("chkbxBauplaeneVorhanden"),
-//                  view.byId("chkbxEigenmittelnachweis"),
-//                  view.byId("chkbxEinkommensnachweis"),
-//                  view.byId("chkbxGrundbuchVorhanden"),
-//                  view.byId("chkbxIdentitaetsfeststellung"),
-//                  view.byId("chkbxVermoegenVorhanden"),
-//                  view.byId("chkbxEinwilligungDatenschutz"),
-//                  view.byId("chkbxEinwilligungBankauskuenfte"),
-//                  view.byId("chkbxZahlnachweisKindergeld"),
-//                  view.byId("chkbxEinhaltungGWG"),
-//                  view.byId("chkbxAntragVollstRichtigUntrschrbn"),
-//                  view.byId("chkbxKindergeldBescheid"),
-//                  view.byId("chkbxWohnflaechenberechnung")
-///			];
